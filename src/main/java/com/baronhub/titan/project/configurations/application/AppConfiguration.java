ee45729cc2ddf11626ddf2d7482c43c8f49a34d7
André Redondo <aredondo@agap2.pt>
2016-11-29 16:51:40 +0000

    Initial commit

---
+++ src/main/java/com/baronhub/titan/project/configurations/application/AppConfiguration.java
@@ -0,0 +1,50 @@
+package com.baronhub.titan.project.configurations.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Spring Dispatcher Servlet
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.baronhub.titan.project")
+@PropertySource(value = "classpath:TitanProjectConfiguration.properties")
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired private Environment environment;
+    /**
+     * Sets up the view Resolver for the Spring project
+     * @return ViewResolver
+     */
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix(environment.getRequiredProperty("view.directory"));
+        viewResolver.setSuffix(environment.getRequiredProperty("view.suffix"));
+        return viewResolver;
+    }
+
+    /**
+     * Sets up the view Resolver for the Spring project
+     * @return PropertySourcesPlaceholderConfigurer
+     */
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
---
+++ src/main/java/com/baronhub/titan/project/configurations/application/AppInitializer.java
@@ -0,0 +1,42 @@
+package com.baronhub.titan.project.configurations.application;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.SessionTrackingMode;
+import java.util.HashSet;
+
+/**
+ * Replaces web.xml
+ */
+@ComponentScan(basePackages = "com.baronhub.titan.project")
+public class AppInitializer implements WebApplicationInitializer {
+
+    /**
+     * Application Startup
+     */
+    public void onStartup(ServletContext container) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfiguration.class);
+        ctx.setServletContext(container);
+        ctx.refresh();
+
+        container.addListener(new ContextLoaderListener(ctx));
+        container.setInitParameter("defaultHtmlEscape", "true");
+
+        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+
+        /* <!-- Disables URL-based sessions --> */
+        HashSet<SessionTrackingMode> set = new HashSet<>();
+        set.add(SessionTrackingMode.COOKIE);
+        container.setSessionTrackingModes(set);
+    }
+}
